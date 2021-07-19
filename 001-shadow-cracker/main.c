abb31a0c6195b4aa8fe575baab2b67636ec18fb6
coolmsf <coolder.hsl@outlook.com>
2021-07-19 21:21:25 +0800

    single thread version

--- 001-shadow-cracker/main.c
+++ 001-shadow-cracker/main.c
@@ -7,9 +7,6 @@
 #define MAX_BUF 200
 #define MAX_USER 1024
 
-void check_euid();
-unsigned long get_last_index(char *buf, char c);
-
 struct shadow {
 	char username[MAX_BUF];
 	// char encrypt_method[MAX_BUF];
@@ -24,8 +21,11 @@ struct shadow {
 	int unused;
 };
 
-int
-main(int argc, char *argv[])
+void check_euid();
+unsigned long get_last_index(char *buf, char c);
+int crack_pass(char *salt, char *shadow_pass, char *dict_pass);
+
+int main(int argc, char *argv[])
 {
 	if (argc < 3) {
 		fprintf(stderr, "usage: %s [-u user-name] "
@@ -88,39 +88,109 @@ main(int argc, char *argv[])
 	 */
 	struct shadow *p = user_info;
 
+	// n users
 	while ((nread = getline(&line, &len, fp_shadow)) != -1 && k < MAX_USER) {
 		char *buf;
 		int c = 0;
 		unsigned long index = 0;
+
+		k++;
 		buf = strtok(line, ":");
+
+		// mark:$6$.n.:17736:0:99999:7:::
+		// [--] [----] [---] - [---] ----
+		// |      |      |   |   |   |||+-----------> 9. Unused
+		// |      |      |   |   |   ||+------------> 8. Expiration date
+		// |      |      |   |   |   |+-------------> 7. Inactivity period
+		// |      |      |   |   |   +--------------> 6. Warning period
+		// |      |      |   |   +------------------> 5. Maximum password age
+		// |      |      |   +----------------------> 4. Minimum password age
+		// |      |      +--------------------------> 3. Last password change
+		// |      +---------------------------------> 2. Encrypted Password
+		// +----------------------------------------> 1. Username
+		// see, https://linuxize.com/post/etc-shadow-file/
 
 		/*
 		 * read all fields into shadow structure
 		 */
     	while (buf) {
 			switch (c) {
+			// username
 			case 0:
 				strncpy(p->username, buf, MAX_BUF);
+				*(p->username + strlen(p->username)) = '\0';
 				break;
+			// salt and encrypted password
 			case 1:
 				/*
 				 * this field contains salt and encrypted password
 				 */
+				if (strcmp("!!", buf) == 0 || strcmp("!*", buf) == 0)
+					break;
+
 				index = get_last_index(buf, '$');
+				// salt
 				strncpy(p->salt, buf, index);
+				*(p->salt + strlen(p->salt)) = '\0';
+
+				// encrypted password
 				strncpy(p->encrypted_pass, buf + index, strlen(buf) - index);
+				*(p->encrypted_pass + strlen(p->encrypted_pass)) = '\0';
+				break;
+			// get last password change field
+			case 2:
+				p->last_pass_change = atoi(buf);
+				break;
+			case 3:
+				p->min_pass_age = atoi(buf);
+				break;
+			case 4:
+				p->max_pass_age = atoi(buf);
+				break;
+			case 5:
+				p->warning_period = atoi(buf);
+				break;
+			case 6:
+				p->inactivity_period = atoi(buf);
+			case 7:
+				p->exp_date = atoi(buf);
+				break;
+			case 8:
+				p->unused = atoi(buf);
+				break;
+			default:
+				break;
 			}
 
 			// next field
 			c++;
     	    buf = strtok(NULL, ":");
     	}
+    	// printf("username:%s\t salt:%s\t encrypted_pass:%s\t "
+		// 	   "last_pass_change:%d\t min_pass_age:%d\t max_pass_age:%d\t warning_peroid:%d\t int_pero:%d\n",
+		// 	   p->username, p->salt, p->encrypted_pass, p->last_pass_change, p->min_pass_age, p->max_pass_age,
+		// 	   p->warning_period, p->inactivity_period);
+
 		// next user info
 		p++;
 	}
-
-	free(line);
 	fclose(fp_shadow);
+
+	// for every user
+	for (int i = 0; i < MAX_USER; i++) {
+		char* dict_pass;
+
+		// try every passowrd
+		while ((nread = getline(&dict_pass, &len, fp_dict)) != -1) {
+			// remove '\n' at the end of dict_pass
+			*(dict_pass + strlen(dict_pass) - 1) = '\0';
+			if (crack_pass(user_info[i].salt, user_info[i].encrypted_pass, dict_pass)) {
+				printf("PASS FOUND!!!\nusername:%s pass:%s\n", user_info[i].username, dict_pass);
+			}
+		}
+		rewind(fp_dict);
+	}
+
 	fclose(fp_dict);
 	exit(EXIT_FAILURE);
 }
@@ -150,4 +220,25 @@ unsigned long get_last_index(char *buf, char c)
 	}
 
 	return index;
+}
+
+int crack_pass(char *salt, char *shadow_pass, char *dict_pass)
+{
+	char *encrypted_pass;
+	char s[MAX_LINE] = { 0 };
+
+	if (strcmp(salt, "") == 0 || strcmp(shadow_pass, "") == 0
+	|| strcmp(dict_pass, "") == 0)
+		return 0;
+
+	encrypted_pass = crypt(dict_pass, salt);
+
+	strcat(s, salt);
+	strcat(s, shadow_pass);
+
+	// return (strcmp(encrypted_pass, shadow_pass) == 0) ? 1 : 0;
+	if (strcmp(encrypted_pass, s) == 0)
+		return 1;
+	else
+		return 0;
 }
